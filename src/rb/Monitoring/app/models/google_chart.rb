9a8f591d48a965c33cc24728b5dd9ca2e1da8f80
Vincent Charles <vjcharles@gmail.com>
2010-05-16 19:45:34 +0200

    google_chart module created as a home for common charting functions

---
+++ src/rb/Monitoring/app/models/google_chart.rb
@@ -0,0 +1,49 @@
+#All google chart generation methods now live here (not the perm home for this module)
+module GoogleChart
+  
+  # chart related functions
+  
+  #todo: clean this up. manually generated google chart (probably deleting google chart plugin soon...not needed)
+  def generate_chart(data_array, selected_sort, selected_index, selected_data, time_interval, sorted_tables)
+    smallest = find_smallest(data_array)
+    largest = find_largest(data_array)
+    
+    # handcrafted googlechart url
+    chart = "http://chart.apis.google.com/chart?" + 
+      "cht=bhs&" + 
+      "chts=FF0000,15&" +
+      "chs=500x170&" + # size
+      "chd=t:#{data_array.join(',')}&" + #data 
+      "chds=#{smallest},#{largest}&" + # scale
+      "chxr=0,#{smallest},#{largest}&" + # values to be listed (high and low)
+      "chxt=x,y&" + 
+      "chxl=1:|#{Table.get_all_names(sorted_tables).reverse.map{|n| "name " + n.titleize}.join('|')}&" + #notice the order is reversed, put stat label here
+      "chco=FF0000&"
+    if selected_sort == "name"  
+      chart = chart + "chtt=#{selected_data.titleize}, sorted by #{selected_sort.titleize}|every " + (time_interval[selected_index] > 1 ? "#{time_interval[selected_index]} seconds" : 'second') #title
+    else
+      chart = chart + "chtt=Sorted by #{selected_data.titleize}|every " + (time_interval[selected_index] > 1 ? "#{time_interval[selected_index]} seconds" : 'second') #title
+    end
+    chart
+  end
+  
+  
+  def json_map(chart)
+    chart_map = URI.encode(chart + "&chof=json")
+    chart_map = `curl "#{chart_map}"`
+    chart_map = JSON.parse(chart_map)
+    return chart_map
+  end
+  
+  def generate_html_map(json_map)
+    ">>html map here<<"
+  end
+  
+  # used in view template and controller
+  def map_name
+    return "generic_map_name" unless @selected_sort && @selected_data && @selected_index
+    return "#{@selected_sort}_#{@selected_data}_#{@selected_index}"
+  end
+  
+  
+end
