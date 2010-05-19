baaa1a4a5d94b8de7a3853a5bd8f31e590c0f28d
Vincent Charles <vjcharles@gmail.com>
2010-05-20 01:28:40 +0200

    main controller now show totaled table data in an html table...minimal formatting on larger numbers, and data put in a partial for later ajax rendering.

--- src/rb/Monitoring/app/controllers/main_controller.rb
+++ src/rb/Monitoring/app/controllers/main_controller.rb
@@ -2,22 +2,13 @@ class MainController < ApplicationController
   use_google_charts
   
   def index
-    # @rrd_google_chart_test = rrd_google_charts_test
-        
+    @time_interval = [1, 5, 10]
+    #via table data
+    @timestamps, @system_totals = Table.get_system_totals
     respond_to do |format|
       format.html # index.html.erb
     end
   end
- 
-  # def rrd_google_charts_test
-  #   chart = GoogleLineChart.new :width => 400, :height => 200, :title => ["dataset 1", "dataset 2"]#, :chart_type => GooglePieChart::PIE_3D
-  #   dataset1 = GoogleChartDataset.new :data => get_rrd_data("vtest.rrd", 920804400, 920808000), :color => "FF0000", :title => "dataset 1"
-  #   dataset2 = GoogleChartDataset.new :data => get_rrd_data("vtest.rrd", 920804400, 920808000), :color => "0000FF", :title => "dataset 2"
-  #   data = GoogleChartData.new :datasets => [dataset1, dataset2] 
-  #   axis = GoogleChartAxis.new :axis  => [GoogleChartAxis::LEFT, GoogleChartAxis::BOTTOM,  GoogleChartAxis::RIGHT, GoogleChartAxis::BOTTOM]
-  #   chart.data = data   
-  #   return chart
-  # end  
     
   #not working
   def get_rrd_data(rrd, start_time, end_time)
